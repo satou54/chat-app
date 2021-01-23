6d8f738cece8d08279dfd24475aa5f08caa405e9
satou54 <satou54satou@gmail.com>
2021-01-24 01:16:11 +0900

    入力されたメッセージをリアルタイムで反映できるよう実装

--- app/assets/javascripts/channels/room.js
+++ app/assets/javascripts/channels/room.js
@@ -1,6 +1,5 @@
 App.room = App.cable.subscriptions.create("RoomChannel", {
   connected: function() {
-    // Called when the subscription is ready for use on the server
     console.log('connected')
   },
 
@@ -8,11 +7,23 @@ App.room = App.cable.subscriptions.create("RoomChannel", {
     // Called when the subscription has been terminated by the server
   },
 
-  received: function(data) {
-    // Called when there's incoming data on the websocket for this channel
+  received: function(message) {
+    const messages = document.getElementById('messages')
+    messages.innerHTML += '<p>${message}</p>'
   },
 
-  spesk: function() {
-    return this.perform('spesk', {message: "Hello"});
+  speak: function(content) {
+    // messageに引数のcontentで受け取った値を渡す
+    return this.perform('speak', {message: content});
   }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  const input = document.getElementById('chat-input')
+  const button = document.getElementById('button')
+  button.addEventListener('click', function() {
+    const content = input.value
+    App.room.speak(content)
+    input.value = ''
+  })
+})
--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,13 +1,14 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
-
+    stream_from "room_channel"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def spesk
+  def speak(data)
+    Message.create!(content: data["message"])
+    ActionCable.server.broadcast "room_channel", data["message"]
   end
 end
