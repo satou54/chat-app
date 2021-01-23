8ca95aadd764cbd74af6a1e90f2151bc28ea5096
satou54 <satou54satou@gmail.com>
2021-01-24 01:02:49 +0900

    roomチャンネル作成

---
+++ app/assets/javascripts/channels/room.js
@@ -0,0 +1,18 @@
+App.room = App.cable.subscriptions.create("RoomChannel", {
+  connected: function() {
+    // Called when the subscription is ready for use on the server
+    console.log('connected')
+  },
+
+  disconnected: function() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received: function(data) {
+    // Called when there's incoming data on the websocket for this channel
+  },
+
+  spesk: function() {
+    return this.perform('spesk', {message: "Hello"});
+  }
+});
